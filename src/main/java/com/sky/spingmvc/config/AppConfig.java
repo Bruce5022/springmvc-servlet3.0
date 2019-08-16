599d38b5e4fe5529b5124ea726617c6cc52a70e8
syzx-szw <shizhanwei123@126.com>
2019-08-17 00:15:41 +0800

    原始mvc测试代码 szw

--- src/main/java/com/sky/spingmvc/config/AppConfig.java
+++ src/main/java/com/sky/spingmvc/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 
 // Spring的容器只扫描controller：子容器
-@ComponentScan(value = "com.sky.spingmvc", excludeFilters = {
+@ComponentScan(value = "com.sky.spingmvc", includeFilters = {
         @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
 }, useDefaultFilters = false)
 public class AppConfig {
